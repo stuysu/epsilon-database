9f744e28595ad528d3840303076647711670f876
Randy <57469078+GandyT@users.noreply.github.com>
2024-02-10 10:47:32 -0500

    Create POPULATE_DATABASE.sql

---
+++ POPULATE_DATABASE.sql
@@ -0,0 +1,12 @@
+-- populate database with fake data
+INSERT INTO organizations (name, url, state)
+VALUES('SUIT', 'suit', 'ADMIN');
+
+INSERT INTO users(first_name, last_name, email, grad_year)
+VALUES('Randy', 'Sim', 'rsim40@stuy.edu', 2024);
+
+INSERT INTO rooms
+  (name, floor)
+VALUES
+  ('303', 3),
+  ('403', 3);
