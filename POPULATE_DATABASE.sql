b0c6864b7032ba9dff894e691d36e7896cafd13c
Randy <57469078+GandyT@users.noreply.github.com>
2024-02-10 16:51:37 -0500

    Update POPULATE_DATABASE.sql

--- POPULATE_DATABASE.sql
+++ POPULATE_DATABASE.sql
@@ -2,8 +2,11 @@
 INSERT INTO organizations (name, url, state)
 VALUES('SUIT', 'suit', 'ADMIN');
 
-INSERT INTO users(first_name, last_name, email, grad_year)
-VALUES('Randy', 'Sim', 'rsim40@stuy.edu', 2024);
+INSERT INTO users
+  (first_name, last_name, email, grad_year)
+VALUES
+  ('Randy', 'Sim', 'rsim40@stuy.edu', 2024),
+  ('IT', 'Department', 'itdepartment@stuysu.org', 2023);
 
 INSERT INTO rooms
   (name, floor)
