321233b07c69709e6ef01ed9f8e75b4b3a73c5e0
Randy <57469078+GandyT@users.noreply.github.com>
2024-02-20 22:39:04 -0500

    Better database populate

--- POPULATE_DATABASE.sql
+++ POPULATE_DATABASE.sql
@@ -13,3 +13,33 @@ INSERT INTO rooms
 VALUES
   ('303', 3),
   ('403', 3);
+
+INSERT INTO memberships 
+  (organization_id, user_id, role, active)
+VALUES
+  (
+    (SELECT id FROM users WHERE email='rsim40@stuy.edu'),
+    (SELECT id FROM organizations WHERE name='SUIT'),
+    'CREATOR',
+    true
+  );
+
+INSERT INTO meetings 
+  (organization_id, room_id, title, description, start_time, end_time)
+VALUES
+  (
+    (SELECT id FROM organizations WHERE name='SUIT'),
+    (SELECT id FROM rooms WHERE name='303'),
+    'First meeting guys!',
+    'This is our first meeting, I hope you will come',
+    CURRENT_TIMESTAMP,
+    CURRENT_TIMESTAMP
+  );
+
+INSERT INTO permissions 
+  (user_id, permission)
+VALUES
+  (
+    (SELECT id FROM users WHERE email='rsim40@stuy.edu'),
+    'ADMIN'
+  );
