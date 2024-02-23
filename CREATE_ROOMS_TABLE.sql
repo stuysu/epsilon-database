174e556a304db1f8b451b3803d59a4491beaaf47
Randy <57469078+GandyT@users.noreply.github.com>
2024-02-23 16:55:01 -0500

    Fix room perms for site admins

--- CREATE_ROOMS_TABLE.sql
+++ CREATE_ROOMS_TABLE.sql
@@ -46,4 +46,19 @@ USING (
       )
     )
   )
+)
+WITH CHECK (
+  EXISTS (
+    SELECT 1
+    FROM permissions as p
+    INNER JOIN users as u ON (p.user_id = u.id)
+    WHERE (
+      u.email = auth.jwt() ->> 'email'
+      AND ( -- roles here
+        p.permission = 'ADMIN'
+      )
+    )
+  )
 );
+
+
