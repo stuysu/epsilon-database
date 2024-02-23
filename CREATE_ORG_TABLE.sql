42053f602445b17e71fb08b345d89348d45d9466
Randy <57469078+GandyT@users.noreply.github.com>
2024-02-23 16:47:33 -0500

    Enable all org access to site admins

--- CREATE_ORG_TABLE.sql
+++ CREATE_ORG_TABLE.sql
@@ -95,3 +95,22 @@ USING (
     )
   )
 );
+
+CREATE POLICY "Enable all access to site admins"
+ON public.organizations
+FOR ALL
+TO authenticated
+USING (
+  EXISTS (
+    SELECT 1
+    FROM permissions as p
+    INNER JOIN users as u ON (p.user_id = u.id)
+    WHERE (
+      u.email = auth.jwt() ->> 'email'
+      AND ( -- roles here
+        p.permission = 'ADMIN'
+      )
+    )
+  )
+);
+
