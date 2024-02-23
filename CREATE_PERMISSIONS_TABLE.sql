0b5308397a4256334b0f53216c5d1b9133be47da
Randy <57469078+GandyT@users.noreply.github.com>
2024-02-22 21:47:38 -0500

    Update requirements for permission table

--- CREATE_PERMISSIONS_TABLE.sql
+++ CREATE_PERMISSIONS_TABLE.sql
@@ -9,8 +9,8 @@ END $$;
 
 CREATE TABLE permissions (
   id SERIAL PRIMARY KEY,
-  user_id INT,
-  permission site_perms,
+  user_id INT NOT NULL,
+  permission site_perms NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   FOREIGN KEY (user_id) REFERENCES users(id),
