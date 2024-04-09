98a678a743b4d044fb2bf2aa1a27a6c5444bd65a
Randy <57469078+randysim@users.noreply.github.com>
2024-04-09 16:49:10 -0400

    Added perms for getting booked rooms

--- DATABASE_FUNC_PERMS.sql
+++ DATABASE_FUNC_PERMS.sql
@@ -5,3 +5,4 @@ GRANT EXECUTE ON FUNCTION add_creator TO postgres; -- let database add creator t
 GRANT EXECUTE ON FUNCTION get_user_admin_organizations TO postgres; -- get organizations user is admin of
 GRANT EXECUTE ON FUNCTION get_user_creator_organizations TO postgres; -- get organizations user is creator of 
 GRANT EXECUTE ON FUNCTION is_admin TO postgres; -- check if user is admin of site
+GRANT EXECUTE ON FUNCTION get_booked_rooms TO authenticated; -- allow club leaders (those signed in) to get booked rooms within a timestamp
