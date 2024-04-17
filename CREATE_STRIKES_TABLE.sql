3b967e4b2a27709e3191f348d864d52f92c9fd31
Randy <57469078+randysim@users.noreply.github.com>
2024-04-16 20:09:58 -0400

    Create CREATE_STRIKES_TABLE.sql

---
+++ CREATE_STRIKES_TABLE.sql
@@ -0,0 +1,66 @@
+CREATE TABLE strikes (
+  id SERIAL PRIMARY KEY,
+  organization_id INT NOT NULL,
+  admin_id INT NOT NULL, -- the person who handed out the strike
+  reason TEXT,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (organization_id) REFERENCES organizations(id),
+  FOREIGN KEY (admin_id) REFERENCES users(id)
+);
+
+create extension if not exists moddatetime schema extensions;
+
+-- trigger to update "updated_at" field before every update to row
+create trigger handle_updated_at before update on strikes
+  for each row execute procedure moddatetime (updated_at);
+
+ALTER TABLE strikes ENABLE ROW LEVEL SECURITY;
+
+CREATE POLICY "Enable read access to admins of organization"
+ON public.strikes
+FOR SELECT
+TO authenticated
+USING (
+  EXISTS (
+    SELECT 1
+    FROM users as u
+    INNER JOIN memberships as m ON (u.id = m.user_id)
+    WHERE (
+      u.email = (auth.jwt() ->> 'email')
+      AND m.organization_id = organization_id
+      AND (m.role = 'ADMIN' OR m.role = 'CREATOR')
+    )
+  )
+);
+
+CREATE POLICY "Enable all access to site admins"
+ON public.strikes
+FOR ALL
+TO authenticated
+USING (
+  EXISTS (
+    SELECT 1
+    FROM permissions as p
+    INNER JOIN users as u ON (p.user_id = u.id)
+    WHERE (
+      u.email = auth.jwt() ->> 'email'
+      AND ( -- roles here
+        p.permission = 'ADMIN'
+      )
+    )
+  )
+)
+WITH CHECK (
+  EXISTS (
+    SELECT 1
+    FROM permissions as p
+    INNER JOIN users as u ON (p.user_id = u.id)
+    WHERE (
+      u.email = auth.jwt() ->> 'email'
+      AND ( -- roles here
+        p.permission = 'ADMIN'
+      )
+    )
+  )
+);
