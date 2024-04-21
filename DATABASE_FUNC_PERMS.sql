d3f24601c7094cb961f7148f27528067a693a959
Randy <57469078+randysim@users.noreply.github.com>
2024-04-21 19:13:15 -0400

    Update DATABASE_FUNC_PERMS.sql

--- DATABASE_FUNC_PERMS.sql
+++ DATABASE_FUNC_PERMS.sql
@@ -6,3 +6,4 @@ GRANT EXECUTE ON FUNCTION get_user_admin_organizations TO postgres; -- get organ
 GRANT EXECUTE ON FUNCTION get_user_creator_organizations TO postgres; -- get organizations user is creator of 
 GRANT EXECUTE ON FUNCTION is_admin TO postgres; -- check if user is admin of site
 GRANT EXECUTE ON FUNCTION get_booked_rooms TO authenticated; -- allow club leaders (those signed in) to get booked rooms within a timestamp
+GRANT EXECUTE ON FUNCTION get_random_organizations TO anon;
