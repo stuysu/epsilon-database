0b417c158783b087b23c707978b97e4e2ff624a7
Randy <57469078+randysim@users.noreply.github.com>
2024-05-01 21:41:09 -0400

    Create CREATE_SETTINGS_TABLE.sql

---
+++ CREATE_SETTINGS_TABLE.sql
@@ -0,0 +1,50 @@
+CREATE TABLE settings(
+    Lock char(1) not null DEFAULT 'X', -- make this column contain only 1 value, but also be unique to create only 1 row
+    /* Other columns */
+    required_members INT NOT NULL DEFAULT 0,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    /* constraints */
+    constraint PK_T1 PRIMARY KEY (Lock),
+    constraint CK_T1_Locked CHECK (Lock='X')
+);
+
+create extension if not exists moddatetime schema extensions;
+
+-- trigger to update "updated_at" field before every update to row
+create trigger handle_updated_at before update on settings
+  for each row execute procedure moddatetime (updated_at);
+
+ALTER TABLE settings ENABLE ROW LEVEL SECURITY;
+
+CREATE POLICY "Enable all access to site admins"
+ON public.settings
+FOR ALL
+TO authenticated
+USING (
+  EXISTS (
+    SELECT 1
+    FROM permissions as p
+    INNER JOIN users as u ON (p.user_id = u.id)
+    WHERE (
+      u.email = auth.jwt() ->> 'email'
+      AND ( -- roles here
+        p.permission = 'ADMIN'
+      )
+    )
+  )
+)
+WITH CHECK (
+  EXISTS (
+    SELECT 1
+    FROM permissions as p
+    INNER JOIN users as u ON (p.user_id = u.id)
+    WHERE (
+      u.email = auth.jwt() ->> 'email'
+      AND ( -- roles here
+        p.permission = 'ADMIN'
+      )
+    )
+  )
+);
