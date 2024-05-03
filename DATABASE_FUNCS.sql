daa83782cfb742f4f8e79823e2d8745db61a80b1
Randy <57469078+randysim@users.noreply.github.com>
2024-05-03 19:44:55 -0400

    Update DATABASE_FUNCS.sql

--- DATABASE_FUNCS.sql
+++ DATABASE_FUNCS.sql
@@ -1,14 +1,15 @@
 -- returns ids of rooms that are unavailable
 -- frontend will filter out all rooms using that information
 create or replace function get_booked_rooms(meeting_start TIMESTAMP, meeting_end TIMESTAMP)
-returns setof int
+returns TABLE (room_id INT, meeting_id INT)
 language sql
 security definer
 set search_path = public
 stable
 AS $$
-  SELECT room_id
-  FROM meetings
+  SELECT r.id, m.id
+  FROM rooms AS r
+  INNER JOIN meetings as m ON (m.room_id = r.id)
   WHERE (
     (
       start_time >= meeting_start and
