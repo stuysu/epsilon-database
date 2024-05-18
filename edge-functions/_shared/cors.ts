282e17ce3bb4b423bc2fe0f8e23d75e5b181cd87
Randy <57469078+randysim@users.noreply.github.com>
2024-05-18 10:46:21 -0400

    Create cors.ts

---
+++ edge-functions/_shared/cors.ts
@@ -0,0 +1,7 @@
+/* reusable corsHeaders */
+const corsHeaders = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
+}
+
+export default corsHeaders;
