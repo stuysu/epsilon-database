bbcbbb68a1332eb6f9eb7baa35bd3d4fb004558e
Randy <57469078+randysim@users.noreply.github.com>
2024-06-20 20:32:40 -0400

    Delete edge-functions directory
    moved to 
    https://github.com/stuysu/epsilon-functions

--- edge-functions/_shared/cors.ts
+++
@@ -1,7 +0,0 @@
-/* reusable corsHeaders */
-const corsHeaders = {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
-}
-
-export default corsHeaders;
