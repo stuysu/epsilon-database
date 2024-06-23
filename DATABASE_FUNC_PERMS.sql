e0ecc91a0274ab3ad1e2ad23356466fb6c452a66
Randy <57469078+randysim@users.noreply.github.com>
2024-06-23 14:29:48 -0400

    Update DATABASE_FUNC_PERMS.sql

--- DATABASE_FUNC_PERMS.sql
+++ DATABASE_FUNC_PERMS.sql
@@ -8,3 +8,4 @@ GRANT EXECUTE ON FUNCTION is_admin TO postgres; -- check if user is admin of sit
 GRANT EXECUTE ON FUNCTION get_booked_rooms TO authenticated; -- allow club leaders (those signed in) to get booked rooms within a timestamp
 GRANT EXECUTE ON FUNCTION get_random_organizations TO anon;
 GRANT EXECUTE ON FUNCTION update_profile_picture TO authenticated;
+GRANT EXECUTE ON FUNCTION get_unique_meeting_days TO anon;
