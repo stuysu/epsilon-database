bec8dddd40ebe5a91025db739915cc36945b5b06
Randy <57469078+randysim@users.noreply.github.com>
2024-06-28 18:56:50 -0400

    Update CREATE_GOOGLETOKENS.sql

--- CREATE_GOOGLETOKENS.sql
+++ CREATE_GOOGLETOKENS.sql
@@ -13,7 +13,7 @@ CREATE TABLE backgroundtokens(
 create extension if not exists moddatetime schema extensions;
 
 -- trigger to update "updated_at" field before every update to row
-create trigger handle_updated_at before update on googletokens
+create trigger handle_updated_at before update on backgroundtokens
   for each row execute procedure moddatetime (updated_at);
 
-ALTER TABLE googletokens ENABLE ROW LEVEL SECURITY;
+ALTER TABLE backgroundtokens ENABLE ROW LEVEL SECURITY;
