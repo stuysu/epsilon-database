fecac6cb61f9a6906cb75d2027a5f7dcf0e41b62
David Chen <dchen07@protonmail.com>
2024-08-18 16:12:18 -0400

    Add four digit ID table with RLS
    Separate table was added to avoid the complexity of CLS, uses user_id as
    both the primary key ID and the foreign key linking to the users table.
    
    Unique constraints on both columns to prevent mistaken records.

---
+++ 024_CREATE_FOURDIGITID_TABLE.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS fourdigitids (
+  user_id integer NOT NULL UNIQUE,
+  PRIMARY KEY (user_id),
+  FOREIGN KEY (user_id) REFERENCES users(id) on delete cascade,
+  value smallint UNIQUE
+);
+
+ALTER TABLE fourdigitids ENABLE row level security;
+
+CREATE POLICY "Users can view their own four digit IDs."
+ON "public"."fourdigitids"
+FOR SELECT
+TO authenticated
+USING (
+  (EXISTS
+    ( SELECT 1
+      FROM users u
+      WHERE ((u.email = (jwt() ->> 'email'::text)) AND (fourdigitids.user_id = u.id))
+    )
+  )
+);
