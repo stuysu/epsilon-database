0f6b00b59565aa41fc06d6f6a508c5531145a56a
David Chen <dchen07@protonmail.com>
2024-10-06 15:23:02 -0400

    Add row level security

--- 027_CREATE_NOTIFICATIONS.sql
+++ 027_CREATE_NOTIFICATIONS.sql
@@ -11,3 +11,22 @@ FROM memberships;
 
 ALTER TABLE memberships
 DROP COLUMN allow_notifications;
+
+ALTER TABLE membershipnotifications ENABLE ROW LEVEL SECURITY;
+
+
+-- SELECT policy implicitly applies to UPDATE policy as well
+CREATE POLICY "Alow users to read/edit their own records"
+ON public.membershipnotifications
+FOR all
+TO authenticated
+USING (
+  EXISTS (
+    SELECT 1 FROM users WHERE (
+      users.email = auth.jwt() ->> 'email' AND
+      users.id = (
+        SELECT user_id FROM memberships WHERE id = membership_id
+      )
+    )
+  )
+);
