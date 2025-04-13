3829debeeab4284c544968b9e9f2c710a419d9e0
Rahuldeb5 <rahuldeb.usa@gmail.com>
2025-04-13 16:09:26 -0400

    Added query for insert policy on users table

---
+++ 030_ADD_USERS.sql
@@ -0,0 +1,13 @@
+CREATE POLICY "Allow insert for site admin"
+ON public.users
+TO authenticated
+WITH CHECK(
+  EXISTS (
+    SELECT 1
+    FROM users u
+    JOIN permissions p ON u.id = p.user_id
+    WHERE
+      u.email = (jwt() ->> 'email')
+      AND p.permission = 'ADMIN'
+  )
+);
